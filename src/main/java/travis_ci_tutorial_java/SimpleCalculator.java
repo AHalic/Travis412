5f32da88d87f6e08709c858bd27018a4738b5898
Sam M <119554204+Sammm5@users.noreply.github.com>
2026-02-27 15:23:30 -0500

    implemented multiply in calculator class

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -7,5 +7,5 @@ public class SimpleCalculator {
 
 	public int minus (int a, int b) { return a - b; }
 
-//	public int multiply (int a, int b) {return a * b;}
+	public int multiply (int a, int b) {return a * b;}
 }
